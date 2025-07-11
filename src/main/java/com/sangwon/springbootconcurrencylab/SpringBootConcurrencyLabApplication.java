0e091796a81a66b1e832ffca9959268af98e5f0e
sangwonlee06 <sangwonlee0622@outlook.com>
2025-07-10 22:27:11 -0700

    Initial commit

---
+++ src/main/java/com/sangwon/springbootconcurrencylab/SpringBootConcurrencyLabApplication.java
@@ -0,0 +1,13 @@
+package com.sangwon.springbootconcurrencylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConcurrencyLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConcurrencyLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sangwon/springbootconcurrencylab/SpringBootConcurrencyLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangwon.springbootconcurrencylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConcurrencyLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
